2cd3a403c57b7ce850c053f2aadba335f415634e
peterjames3 <kamauchief97@gmail.com>
2025-06-29 16:51:53 +0300

    first commit

--- app/(site)/exam-aid/[slug]/page.tsx
+++ app/(site)/exam-aid/[slug]/page.tsx
@@ -1,28 +1,28 @@
-import { notFound } from "next/navigation";
-import ExamPageClient from "@/app/ui/components/exam-aid/exam-page-client";
-import { getExamData, getAllExamSlugs } from "@/lib/datafetching/exam-service";
-//import type { Metadata } from "next";
+// import { notFound } from "next/navigation";
+// import ExamPageClient from "@/app/ui/components/exam-aid/exam-page-client";
+// import { getExamData, getAllExamSlugs } from "@/lib/datafetching/exam-service";
+// //import type { Metadata } from "next";
 
-export type paramsType = Promise<{ slug: string }>;
-type Props = {
-  params: paramsType;
-};
+// export type paramsType = Promise<{ slug: string }>;
+// type Props = {
+//   params: paramsType;
+// };
 
-export default async function ExamAidPage({ params }: Props) {
-  const { slug } = await params;
-  const exam = await getExamData(slug);
-  console.log(exam);
+// export default async function ExamAidPage({ params }: Props) {
+//   const { slug } = await params;
+//   const exam = await getExamData(slug);
+//   console.log(exam);
 
-  if (!exam) {
-    notFound();
-  }
+//   if (!exam) {
+//     notFound();
+//   }
 
-  return <ExamPageClient exam={exam} />;
-}
+//   return <ExamPageClient exam={exam} />;
+// }
 
- export async function generateStaticParams() {
-   const slugs = await getAllExamSlugs();
-   return slugs.map((slug) => ({ slug }));
- }
+//  export async function generateStaticParams() {
+//    const slugs = await getAllExamSlugs();
+//    return slugs.map((slug) => ({ slug }));
+//  }
 
-export const dynamicParams = false;
+// export const dynamicParams = false;

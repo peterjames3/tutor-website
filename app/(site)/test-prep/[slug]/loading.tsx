658953f34dd21ab9f745e11e55c55ff645dcb8cb
peterjames3 <kamauchief97@gmail.com>
2025-06-29 17:12:13 +0300

    first commit

--- app/(site)/test-prep/[slug]/loading.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/test-prep/[slug]/loading.tsx
-export default function LoadingTestPrep() {
-  return (
-    <div className="max-w-3xl mx-auto p-4 min-h-screen">
-      <p className="animate-pulse text-gray-400">Loading test details…</p>
-    </div>
-  );
-}

6357289c6a15bd5cea12bcc80dc8c5db852f57b8
peterjames3 <kamauchief97@gmail.com>
2025-09-22 13:13:16 +0300

    updated the robot.ts file  to include the actual location of my xml file

--- app/robot.ts
+++ app/robot.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://sunsetbrightEnergyLimited.com/sitemap.xml",
+    sitemap: "https://testhelpnow.com/sitemap.xml",
   };
 }
